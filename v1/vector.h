f421a52303665fe8a26bde11328c91debcf1c770
Alicia <aliciasun0102@gmail.com>
2026-01-13 01:31:14 -0500

    Starting second impl

---
+++ v1/vector.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace MyImpl {
+
+    template <class T>
+    class Vector {
+    
+    public:
+        
+    private:
+        T* vec_start_;
+        T* vec_end_;
+        T* alloc_end_;
+    };
+}
